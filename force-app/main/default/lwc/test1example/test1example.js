3dbc43d85033605c30951088f36aac7ee2281e5e
durgaram-44 <rambabu.unoscm@gmail.com>
2023-10-13 15:31:58 +0530

    Tabsdispayed11

---
+++ force-app/main/default/lwc/test1example/test1example.js
@@ -0,0 +1,31 @@
+import { LightningElement } from "lwc";
+export default class Test1example extends LightningElement {
+  carList = ["Ford", "Audi", "Maruti", "Hyundai", "Mercedes"];
+  programmingList = [
+    {
+      id: "06868",
+      language: "HTML"
+    },
+    {
+      id: "19797",
+      language: "CSS"
+    },
+    {
+      id: "298789",
+      language: "Javascript"
+    },
+    {
+      id: "398798",
+      language: "Apex"
+    },
+    {
+      id: "48967",
+      language: "Aura"
+    },
+    {
+      id: "58798",
+      language: "Java"
+    }
+  ];
+}
+
